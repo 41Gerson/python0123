bed8b495b12602482d5986da8b9f1bb68ac93e7b
41Gerson <122193894+41Gerson@users.noreply.github.com>
2023-01-21 02:30:40 -0500

    Semana1Tarea

---
+++ TareaSemana1.py
@@ -0,0 +1,134 @@
+'''
+Nombre: Gerson Jhair Vasquez Inga
+'''
+
+'''
+Pregunta 1
+Realizar un programa que ingrese sus datos personales e imprimirlos
+'''
+print("PREGUNTA 1")
+
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+dni = input("Ingrese su numero de DNI: ")
+num_celular = input("Ingrese su numero de celular: ")
+est_civil = input("Ingrese su estado civil: ")
+correo = input("Ingrese su correo personal: ")
+
+print("**DATOS PERSONALES**")
+print("Nombre: "+nombre)
+print("Apellido: "+apellido)
+print("DNI: "+dni)
+print("Celular: "+num_celular)
+print("Estado civil: "+est_civil)
+print("Correo: "+correo)
+
+
+
+'''
+Pregunta 2
+Calcule el área de un círculo, triángulo y cuadrado con radio ingresado desde el
+teclado.
+'''
+print("PREGUNTA 2")
+radio = float(input("Ingrese el radio del circulo: "))
+lado_triangulo = float(input("Ingrese el lado del triangulo: "))
+lado_cuadrado = float(input("Ingrese el lado del cuadrado: "))
+
+import math
+area_circulo = round(math.pi*pow(radio,2),2)
+area_triangulo = round(pow(3,1/2)*pow(lado_triangulo,2)/4,2)
+area_cuadrado = round(lado_cuadrado*lado_cuadrado,2)
+
+print(f"Area del circulo: {area_circulo} u2")
+print(f"Area del triangulo: {area_triangulo} u2")
+print(f"Area del cuadrado: {area_cuadrado} u2")
+
+'''
+Pregunta 3
+Ingrese 3 valores y realice las operaciones de suma ,resta ,multiplicación, división
+y división entera.
+'''
+print("PREGUNTA 3")
+valor1 = int(input("Ingrese el primer valor: "))
+valor2 = int(input("Ingrese el segundo valor: "))
+valor3 = int(input("Ingrese el tercer valor: "))
+
+print("Suma: ",valor1+valor2+valor3)
+
+print("Resta v1-v2: ",valor1-valor2)
+print("Resta v2-v3: ",valor2-valor3)
+print("Resta v3-v1: ",valor3-valor1)
+
+print("Multiplicacion v1-v2: ",valor1*valor2 )
+print("Multiplicacion v2-v3: ",valor2*valor3 )
+print("Multiplicacion v3-v1: ",valor3*valor1 )
+print("Multiplicacion v1-v2-v3: ",valor1*valor2*valor3 )
+
+print("Division v1-v2: ",valor1/valor2)
+print("Division v2-v3: ",valor2/valor3)
+print("Division v3-v1: ",valor3/valor1)
+
+print("Division entera v1-v2: ",valor1//valor2)
+print("Division entera v2-v3: ",valor2//valor3)
+print("Division entera v3-v1: ",valor3//valor1)
+
+'''
+Pregunta 4
+Ingrese un valor e imprima el tipo de dato
+'''
+print("PREGUNTA 4")
+tipo = input("Ingrese un valor: ")
+print(type(tipo))
+
+'''
+Pregunta 5  
+Realice un programa que imprima la ubicación de su carpeta donde se encuentra
+trabajando.
+'''
+print("PREGUNTA 5")
+import sys
+variable =sys.argv[0]
+print(variable)
+
+'''
+Pregunta 6 
+Realice un programa que calcule la suma de los números hasta el valor ingresado.
+'''
+print("PREGUNTA 6")
+numero = int(input("Ingrese un numero: "))
+suma_numeros = 0
+for i in range(1,numero+1):
+    suma_numeros = suma_numeros+i
+print("La suma total es: ",suma_numeros)
+
+'''
+Pregunta 7 
+Realiza un programa que lea 2 números por teclado 
+'''
+print("PREGUNTA 7")
+num1 = float(input("Numero 1: "))
+num2 = float(input("Numero 2: "))
+if num1==num2:
+    print("Los numeros son iguales")
+else:
+    print("Los numeros no son iguales")
+    if num1>num2:
+        print("El primer numero es mayor")
+    elif num2>num1:
+        print("El segundo numero es mayor")
+
+'''
+Pregunta 8
+Escribir un programa que almacene la cadena de caracteres contraseña en una
+variable, pregunte al usuario por la contraseña e imprima por pantalla si la
+contraseña introducida por el usuario coincide con la guardada en la variable sin
+tener en cuenta mayúsculas y minúsculas.
+'''
+print("PREGUNTA 8")
+contrasenia1 = input("Ingrese una contraseña: ")
+contrasenia2 = input("Ingrese la contraseña: ")
+if contrasenia1==contrasenia2:
+    print("Coincide")
+else:
+    print("No coincide")
---
+++ TareaSemana2.py
@@ -0,0 +1,104 @@
+'''
+Nombre: Gerson Jhair Vasquez Inga
+'''
+
+#Pregunta 1:
+
+print("Menu de opciones")
+print("1-Dibujar un cuadrado")
+print("2-Multiplos de 2")
+print("3-Mayores de edad")
+print("0-Salir")
+opcion=int(input("Ingrese una opcion: "))
+while(opcion!=0):
+    if opcion==1:
+        num_lados=int(input("Ingrese el numero de lados: "))
+        for i in range(1,num_lados+1):
+            for j in range(1,num_lados+1):
+                print("*",end="")
+            print("")
+
+    elif opcion==2:
+        numElementos = int(input("Numero de elementos de la lista: "))
+        listaNum =[]
+        for k in range(1,numElementos+1):
+            print("Numero ",k,": ")
+            numeroAdd = int(input(""))
+            listaNum.append(numeroAdd)
+        print(listaNum)
+
+        for l in range(0,numElementos):
+            if listaNum[l]%2==0:
+                print(listaNum[l],"es un multiplo de 2")
+
+    elif opcion==3:
+        numPersonas = int(input("Numero de personas: "))
+        listaPersonas=[]
+        for i in range(1,numPersonas+1):
+            nombreEdad=[]
+            nombreEdad.clear()
+            print("Nombre de persona ",i,": ")
+            nombrePersonas=input("")
+            print("Edad de persona ",i,": ")
+            edadPersonas=int(input(""))
+            nombreEdad.append(nombrePersonas)
+            nombreEdad.append(edadPersonas)
+            listaPersonas.append(nombreEdad)
+    
+        print("Mayores de edad: ")
+        for m in range(0,len(listaPersonas)):
+            if listaPersonas[m][1]>=18:
+                print(listaPersonas[m][0],"-",listaPersonas[m][1]," años ")
+
+    opcion=int(input("Ingrese una opcion del menu: "))
+
+
+
+
+
+#Problema 2
+
+biblioteca ={
+    'libros':{
+    'La ciudad y los perros':'Mario Vargas Llosa',
+    'Cien años de soledad':'Gabriel Garcia Marquez',
+    'La Metamorfosis':'Franz Kafka',
+    'Poemas humanos':'Cesar Vallejo',
+    'Romeo y Julieta':'William Shakespeare'
+    },
+    'categorias':['comedia','tragedia','novela','drama'],
+    'usuarios':['722','723','724','725']
+}
+
+print("Categorias de los libros: ")
+print(biblioteca['categorias'])
+
+print("Libros:")
+print(biblioteca['libros'].keys())
+print("Autores de libros:")
+print(biblioteca['libros'].values())
+
+print("Usuarios de la biblioteca: ")
+print(biblioteca['usuarios'])
+
+
+
+#Pregunta 3
+def numMayor(num1,num2):
+    if num1>num2:
+        return num1
+    elif num2>num1:
+        return num2
+
+numero1=input("Ingrese el primer numero: ")
+numero2=input("Ingrese el segundo numero: ")
+print("El numero mayor es: ",numMayor(numero1,numero2))
+
+
+#Pregunta 4 
+import sys
+contador = 0
+for i in sys.argv:
+    contador = contador +1
+    print("Argumento {}: {}".format(contador,i))
+
